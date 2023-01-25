d3463eecb5af643c871cbda92212a2616c68a8e9
Starfire1281 <mj.starfire1281@gmail.com>
2023-01-25 13:24:25 -0700

    Add paths script

---
+++ addPaths.m
@@ -0,0 +1,3 @@
+addpath('C:\Users\mgarc\Downloads\Ptychography-master\Ptychography-master')
+addpath('C:\Users\mgarc\Downloads\CameraStage_0.1.2')
+addpath(genpath('C:\Users\mgarc\Downloads\Ptychography-master\Ptychography-master\codedPtychography'))
