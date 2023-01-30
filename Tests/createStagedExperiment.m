6c58a185eaf4fb1ba559665ad4cea18abf76472b
Turboprop03 <jmrostykus@mymail.mines.edu>
2023-01-30 13:44:57 -0700

    swapped matrix elements to match broadcasting standard

--- Tests/createStagedExperiment.m
+++ Tests/createStagedExperiment.m
@@ -14,28 +14,32 @@ imageY = 3840;
 
 %Load pictures into experiment
 imageFiles=dir(path2Images);
+%It should just be /2 for the 2 filetypes, but for some reason it adds an
+%extra file.
 imageNum = (length(imageFiles)/2)-1;
-imageData = zeros(length(imageFiles),imageX,imageY);
-imagePositions = zeros(length(imageFiles),2);
+imageData = zeros(imageX,imageY,length(imageFiles));
+imagePositions = zeros(2,length(imageFiles));
 for i=1:imageNum
     image1 = imread(strcat(path2Images,"/image",string(i-1),".bmp"));
-    imageData(i,:,:) = image1;
+    imageData(:,:,i) = image1;
     if (i ~= 1)
         image2 = imread(strcat(path2Images,"/image",string(i-2),".bmp"));
         twoDshift = findShift2Images(image1,image2);
-        imagePositions(i,:,:) = twoDshift + imagePositions(i-1,:,:);
+        imagePositions(:,i) = twoDshift + imagePositions(:,i-1);
     else
-        imagePositions(i,:,:) = [0,0];
+        imagePositions(:,i) = [0,0];
     end
 end
 ex.dataDiffraction = imageData;
 
-ex.positionsX = imagePositions(:,1);
-ex.positionsY = imagePositions(:,2);
+ex.positionsX = imagePositions(1,:);
+ex.positionsY = imagePositions(2,:);
 
-ex.numberOfPositions = 12;
+ex.numberOfPositions = imageNum;
 ex.numberOfWavelengths = 1;
         
+%Not sure if we should be setting our reconstruction size to our detector
+%size
 ex.reconstructedPixelSizeX = imageX;
 ex.reconstructedPixelSizeY = imageY;
 
