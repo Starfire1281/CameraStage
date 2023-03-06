4d5d296d1e4f61001bbf1169c0297634991fd98e
Turboprop03 <jmrostykus@mymail.mines.edu>
2023-03-06 14:25:15 -0700

    Updated convolveFromFile to find all our convolutions

--- convolveFromFile.m
+++ convolveFromFile.m
@@ -1,7 +1,21 @@
 %Iterate through images in folder
-disp("Please enter the name of the folder you would like to convolve");
+disp("Please select the folder you would like to convolve");
 imagesPath = uigetdir("./ImageFiles");
-imageFiles = files(imagesPath);
-for x = 1:length(files);
-myFun(f[x]);
+disp(imagesPath);
+imageFiles = dir([imagesPath '/*.bmp']);
+%convolutionsPath 
+
+%Open file to write to
+pathSplit=regexp(imagesPath,'\','split');
+fileName = append(char(pathSplit(1,end)),'.txt');
+fid = fopen(append(".\ConvolutionFiles\",fileName), 'wt' );
+fprintf( fid, 'Yay File! \n');
+
+for i = 1:(length(imageFiles)-1)
+    image1 = imread(append(imagesPath,'\',imageFiles(i).name));
+    image2 = imread(append(imagesPath,'\',imageFiles(i+1).name));
+    [a1,a2] = findShift2Images(image1,image2);
+    fprintf( fid, '%f,%f\n', a1, a2);
 end
+
+fclose(fid);
